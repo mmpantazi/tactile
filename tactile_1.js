dd2ab6462c138d5499060b95cda36ac58a05002c
Marius Pantazi <marius.pantazi23@gmail.com>
2022-09-14 19:20:39 +0300

    Initial commit

---
+++ tactile_1.js
@@ -0,0 +1,12 @@
+function sum_of_multiples() {
+    let sum = 0;
+    for (let i = 0; i <= 1000; i++) {
+  
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  console.log( sum_of_multiples() )
