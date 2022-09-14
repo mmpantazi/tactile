c946aadd44948165c8cef18b31fc8b7d019a792c
Marius Pantazi <63082923+mmpantazi@users.noreply.github.com>
2022-09-14 19:37:42 +0300

    Update tactile_1.js

--- tactile_1.js
+++ tactile_1.js
@@ -1,6 +1,6 @@
 function sum_of_multiples() {
     let sum = 0;
-    for (let i = 0; i <= 1000; i++) {
+    for (let i = 0; i < 1000; i++) {
       if (i % 3 === 0 || i % 5 === 0) {
         sum += i;
       }
